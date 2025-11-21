d6e2ae9ffa239d8426a7716511ba33103f7b286e
Zaeem Hussain <zaeemnagri550@gmail.com>
2025-11-21 22:29:51 +0500

    Add admin routes for orders and reviews

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,14 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
+const ordersAdminRoutes = require('./routes/ordersAdmin');
+const reviewsAdminRoutes = require('./routes/reviewsAdmin');
 const contactRoutes = require("./routes/contact");
+
 
 dotenv.config();
 const app = express();
-app.use(cors({ origin: process.env.FRONTEND_URL || 'https://bookstore-mern-z8dg.vercel.app' }));
+app.use(cors({ origin: process.env.FRONTEND_URL || '' }));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
@@ -26,6 +29,10 @@ app.use('/api/cart', require('./routes/cart'));
 app.use('/api/orders', require('./routes/orders'));
 app.use('/api/reviews', require('./routes/reviews'));
 app.use('/api/wishlist', require('./routes/wishlist'));
+app.use('/api/admin/orders', ordersAdminRoutes);
+app.use('/api/admin/reviews', reviewsAdminRoutes);
+
+
 app.use("/api/contact", contactRoutes);
 
 // fallback
